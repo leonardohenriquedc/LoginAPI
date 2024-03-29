df4cdabcfd4a43bc31128d1a64df64434502b672
Leonardo Henrique <leonardohenriqueD20@outlook.com>
2024-03-29 13:21:28 -0400

    Update server.js

---
+++ server.js
@@ -0,0 +1,50 @@
+// server.js 
+
+import fastify from 'fastify';
+
+import { DataBasePost } from './dataBases/dataBaseInicial.js';
+
+const server = fastify();
+
+const dataBasePost = new DataBasePost;
+
+server.post('/create', (request, response) => {
+       const {nome, cpf, senha} = request.body;
+
+       dataBasePost.create({
+        nome,
+        cpf, 
+        senha
+       })
+
+       response.status(201).send()
+})
+
+server.get('/login/:cpf', (request, response) => {
+    const cpf = request.params.cpf
+    const loginRes = dataBasePost.validacionlogin(cpf);
+    if(loginRes){
+        response.send('Foi encontrado')
+    }
+    else{
+        response.send('Não foi encontrado')
+    }
+    console.log(typeof(cpf), cpf)
+}) 
+
+server.get('/listPersons', () =>{
+    return (dataBasePost.listContent())
+})
+
+server.put('/uptadeKey/:cpf', (request, response) => {
+    const cpf = request.query.search;
+    const senha = request.body
+    const value = dataBasePost.update(cpf, senha);
+    console.log(value)
+})
+
+
+
+server.listen({
+    port: 4070
+})
