d3120058bf864d8be76168dfc03036f3e2441476
Leonardo Henrique <leonardohenriqueD20@outlook.com>
2024-04-02 17:27:34 -0400

    adaptando a api

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // server.js 
 import fastify from 'fastify';
 
+import fastifyCors from 'fastify';
+
 import { DataBaseSql } from './dataBases/dataBaseSql.js';
  
 const server = fastify();
@@ -27,13 +29,15 @@ server.post('/create', (request, response) => {
 })
 
 server.post('/login', async (request, response) => {
+    response.header(Access-Control-Allow-Origin, '*')
+
     const { cpf, senha } = request.body;
     console.log(cpf, senha)
     const loginRes = await dataBaseSql.validacionlogin(cpf, senha);
     const conLogin = JSON.parse(loginRes);
     if(conLogin.cpf === cpf){
-        response.send(conLogin);
-        console.log(typeof(conLogin), conLogin.cpf);
+        response.send(JSON.stringify(conLogin));
+        console.log(conLogin)
     }
     else{
         response.send('ta tentando fazer oq safado')
@@ -52,6 +56,26 @@ server.put('/uptadeKey', (request, response) => {
     console.log(value);
 })
 
+server.addHook('onRequest', (req, res, done) => {
+    // Configuração do cabeçalho CORS para permitir solicitações de qualquer origem
+    res.header('Access-Control-Allow-Origin', '*'); // Permite solicitações de qualquer origem
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Métodos permitidos
+    res.header('Access-Control-Allow-Headers', 'Content-Type'); // Cabeçalhos permitidos
+    
+    // Continue para a próxima etapa de manipulação de solicitação
+    done();
+  });
+
+  server.options('*', async (request, response) => {
+    // Adicione os cabeçalhos CORS necessários para responder à solicitação preflight
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    response.header('Access-Control-Allow-Headers', 'Content-Type');
+    response.status(200).send();
+  });
+
 server.listen({
     port: 4070
-})
+});
+
+  
