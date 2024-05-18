31df4a42315fef46e5cbf5d67b047c6157d1f245
Leonardo Henrique <leonardohenriqueD20@outlook.com>
2024-05-18 11:56:38 -0400

    pequeno ajuste

--- config/ConnectionSql.js
+++ config/ConnectionSql.js
@@ -6,4 +6,3 @@ const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 const URL = `postgresql://primeiroBD_owner:DBysk3Y5vmKt@ep-damp-bread-a5htyirz.us-east-2.aws.neon.tech/loginDataBase?sslmode=require`
 
 export const sql = postgres(URL, {ssl: 'import'});
-
